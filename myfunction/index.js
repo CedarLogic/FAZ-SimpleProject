a857d060017ba14d1760b450224b6353eb2d05e8
arafato <oliver.arafat@outlook.com>
2016-05-19 11:45:31 +0200

    initial

---
+++ myfunction/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var moment = require('../lib/node_modules/moment');
+var lib = require('../lib');
+
+module.exports = function (context, req) {
+	context.log('Node.js HTTP trigger function processed a request. RequestUri=%s', req.originalUrl);
+	context.log('Running on ' + process.env.env);
+	context.log(moment().format());
+    if (req.query.name || (req.body && req.body.name)) {
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: lib.hello(req.query.name || req.body.name)
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+    }
+    context.done();
+}
