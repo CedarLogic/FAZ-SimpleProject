a283888e03220e8831e98760ec76ab4c00830b5e
arafato <oliver.arafat@outlook.com>
2016-05-24 20:46:35 +0200

    Update index.js

--- myfunction/index.js
+++ myfunction/index.js
@@ -5,7 +5,7 @@ var lib = require('../lib');
 
 module.exports = function (context, req) {
 	context.log('Node.js HTTP trigger function processed a request. RequestUri=%s', req.originalUrl);
-	context.log('I am running running on ' + process.env.env);
+	context.log('I am running on ' + process.env.env);
 	context.log(moment().format());
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
